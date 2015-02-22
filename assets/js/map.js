15d350091e61491ec53209f038975fba3adac1e7
KITE <ixkaito@gmail.com>
2015-02-22 09:11:47 +0900

    View the heatmap of current time at first

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,33 +1,68 @@
-// d3.jsonの第一引数で読み込むJsonファイルへのパス、第二引数でJsonファイル読み込み後に実行する関数を指定します。
-// 第二引数の無名関数の引数として、errorとjsonを渡しています。errorにはエラー発生時のメッセージ、jsonには読み込み成功時に読み込んだJsonデータが格納されます。
-d3.csv('data.csv', function(error, data){
+/*
+================================================================================
+  Current Time
+================================================================================
+*/
+var now = new Date();
+var hour   = ('0' + now.getHours()).slice(-2);
+var minute = ('0' + now.getMinutes()).slice(-2);
 
-  // Google Mapを初期化してエレメントに適用
-  // 緯度経度は例えばこちらのページを参考（http://imakoko.didit.jp/imakoko_html/memo/map_to_latlang.php）
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 34.2321, lng: 135.1911 },
-    zoom: 11
-  });
+var viewTime = document.getElementById('time');
+viewTime.innerHTML = hour + ':' + minute;
 
-  //ヒートマップ用のデータの作成
-  var pos, heatmapData = [];
-  for (var i = 0; i < data.length; i++) {
-    heatmapData.push({
-      location : new google.maps.LatLng(data[i].lat, data[i].lng),
-      weight : 15
+// Visualize the heatmap of current time
+kakekomiMap(hour, minute);
+
+
+/*
+================================================================================
+  Convert time format to minutes
+================================================================================
+*/
+function time2minute(time) {
+  time = time.split(':');
+  time = parseInt(time[0], 10) * 60 + parseInt(time[1], 10);
+  return time;
+}
+
+/*
+================================================================================
+  Visualize the heatmap
+================================================================================
+*/
+function kakekomiMap(hour, minute) {
+  var time = parseInt(hour, 10) * 60 + parseInt(minute, 10);
+
+  d3.csv('data.csv', function(error, data){
+
+    // Initialize google map
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 34.2321, lng: 135.1911 },
+      zoom: 11
     });
-  }
 
-  //ヒートマップレイヤオブジェクトを作成
-  var heatmap = new google.maps.visualization.HeatmapLayer({
-    radius: 100,
-    opacity: 0.75
+    // Initialize heatmap data
+    var pos, heatmapData = [];
+    for (var i = 0; i < data.length; i++) {
+      if (time2minute(data[i].open) <= time && time <= time2minute(data[i].close)) {
+        heatmapData.push({
+          location : new google.maps.LatLng(data[i].lat, data[i].lng),
+          weight : 10
+        });
+      }
+    }
+
+    // Initialize heatmap layer
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      radius: 100,
+      opacity: 0.75
+    });
+
+    // Set the heatmap data to the heatmap layer
+    heatmap.setData(heatmapData);
+
+    // Overlay the heatmap
+    heatmap.setMap(map);
+
   });
-
-  // ヒートマップレイヤにヒートマップデータを適用
-  heatmap.setData(heatmapData);
-
-  // ヒートマップレイヤをMapオブジェクトに紐付けてMap上に表示
-  heatmap.setMap(map);
-
-});
+}
